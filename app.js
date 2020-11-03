c8e9a11fa7294bc066129b06979b7c9c3f90b917
kriskkkb <krisburk577@gmail.com>
2020-11-03 10:16:54 +0200

    pede

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
+function convertToPixels(n) {
+    if (n > 79) {
+        n = 79
+    }
+    if (n < 0) {
+        n = 0
+    }
+    return n * 8
+}
+function updateSnake() {
+    context.strokeStyle = 'white'
+    context.lineWidth = 1
+
+    snakeCoordinates.forEach(c => {
+        context.fillRect(convertToPixels(c[0]), convertToPixels(c[1]), 8, 8)
+        context.sreokeRect(convertToPixels(c[0]), convertToPixels(c[1]), 8, 8)
+    })
+}
+let snakeCoordinates = [
+    [40, 40],
+    [40, 41],
+    [39, 41],
+    [38, 41]
+]
 const canvas = document.getElementById("gameBoard")
 const context = canvas.getContext("2d")
 
-context.rect(20, 20, 8, 8)
-context.rect(20, 28, 8, 8)
-context.fill()
-
-context.strokeStyle = "white"
-context.lineWidth = 1
-context.strokeRect(20, 20, 8, 8)
-context.strokeRect(20, 28, 8, 8)
-context.stroke()
+updateSnake()
